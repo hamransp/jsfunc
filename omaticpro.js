8d37ceea3d795016a6cec8595bef588195a491c5
Rede <35823221+hamransp@users.noreply.github.com>
2022-11-01 20:41:17 +0800

    Update omaticpro.js

--- omaticpro.js
+++ omaticpro.js
@@ -6,12 +6,12 @@ function autoRelated(data) {
         
         dom += `<div class="card mt-5 ">
         <div class="max-w-full rounded overflow-hidden shadow-sm bg-slate-50">
-            <img class="w-full"
+            <img class="object-cover w-auto bg-center bg-cover h-96"
                 src="https://tse1.mm.bing.net/th?q=${encodeURIComponent(kw)}"
                 alt="${kw}" />
             <div class="px-6 py-4">
-                <div class="font-bold text-xl mb-2"><h3><a href="${baseUrl}search/q=${encodeURIComponent(kw)}">${kw}</a></h3></div>
-                <p class="text-gray-700 text-base">
+                <div class="font-bold text-xl mb-2"><h3 class="capitalize"><a href="${baseUrl}search/q=${encodeURIComponent(kw)}">${kw}</a></h3></div>
+                <p class="text-base text-gray-700 line-clamp-5">
                 <b>${kw}</b> is one of best result for ${postTitle}. Everything in here is for references perpose only. Feel free to save and bookmark ${kw}
                 </p>
             </div>
