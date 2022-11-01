2766a96757619839cd9d2ee1359da54ef5653182
Rede <35823221+hamransp@users.noreply.github.com>
2022-11-01 17:21:26 +0800

    Create omaticprov2.js

---
+++ omaticprov2.js
@@ -0,0 +1,21 @@
+function autoRelated(data) {
+    var out = data[1];		    
+
+    for (i = 0; i < out.length; i++) {
+        var kw = out[i];
+        
+        dom += `<div class="card mt-5 ">
+        <div class="max-w-full rounded overflow-hidden shadow-sm bg-slate-50">
+            <img class="w-full"
+                src="https://tse1.mm.bing.net/th?q=${encodeURIComponent(kw)}"
+                alt="${kw}" />
+            <div class="px-6 py-4">
+                <div class="font-bold text-xl mb-2"><h3><a href="${baseUrl}search/q=${encodeURIComponent(kw)}">${kw}</a></h3></div>
+                <p class="text-gray-700 text-base">
+                <b>${kw}</b> is one of best result for ${postTitle}. Everything in here is for references perpose only. Feel free to save and bookmark ${kw}
+                </p>
+            </div>
+        </div>
+    </div>`;
+    }
+}
