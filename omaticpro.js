ff493da2cd0efe0c2564e3cf22148c898009f0a0
Rede <35823221+hamransp@users.noreply.github.com>
2022-11-01 17:20:06 +0800

    Update omaticpro.js

--- omaticpro.js
+++ omaticpro.js
@@ -2,7 +2,20 @@ function autoRelated(data) {
     var out = data[1];		    
 
     for (i = 0; i < out.length; i++) {
-    	var kw = out[i];
-        dom += `<aside><img alt="${kw}" src="https://tse1.mm.bing.net/th?q=${encodeURIComponent(kw)}" width="320" height="320" style="width:100%;height:auto;margin-right: 8px;margin-bottom: 8px;" /><small>title: <i>${kw}</i></small><h3><a href="${baseUrl}search/q=${encodeURIComponent(kw)}">${kw}</a></h3><p align="justify"><b>${kw}</b> is one of best result for ${postTitle}. Everything in here is for references perpose only. Feel free to save and bookmark ${kw}</p></aside>`;
+        var kw = out[i];
+        
+        dom += `<div class="card mt-5 ">
+        <div class="max-w-full rounded overflow-hidden shadow-sm bg-slate-50">
+            <img class="w-full"
+                src="https://tse1.mm.bing.net/th?q=${encodeURIComponent(kw)}"
+                alt="${kw}" />
+            <div class="px-6 py-4">
+                <div class="font-bold text-xl mb-2"><h3><a href="${baseUrl}search/q=${encodeURIComponent(kw)}">${kw}</a></h3></div>
+                <p class="text-gray-700 text-base">
+                <b>${kw}</b> is one of best result for ${postTitle}. Everything in here is for references perpose only. Feel free to save and bookmark ${kw}
+                </p>
+            </div>
+        </div>
+    </div>`;
     }
 }
